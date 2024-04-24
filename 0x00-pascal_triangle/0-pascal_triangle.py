379d66001782d22e2e8a024cb1d61afc371332c9
robberbadeia <robber.badeia@gmail.com>
2024-04-24 20:11:21 +0200

    Modify code style

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,7 +7,8 @@ def pascal_triangle(n):
     for i in range(n):
         row = [1] * (i + 1)  # Start each row with 1s
         for j in range(1, i):
-            row[j] = triangle[i-1][j-1] + triangle[i-1][j]  # Calculate the inner values of the row
+            row[j] = triangle[i-1][j-1] + \
+                triangle[i-1][j]  # Calculate the inner values of the row
         triangle.append(row)  # Append the completed row to the triangle
 
     return triangle

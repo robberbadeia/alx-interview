53f027f4d57591cc239e3679e4b32ceb98274bf9
robberbadeia <robber.badeia@gmail.com>
2024-06-05 19:46:57 +0300

    Task 0: Adjust code for semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,24 @@
 #!/usr/bin/node
 // Start Wars Charachters
 
-const request = require("request");
+const request = require('request');
 
 const argCounts = process.argv.length;
 if (argCounts < 3) {
-  console.log("Please Enter Movie ID");
+  console.log('Please Enter Movie ID');
 } else {
   const movieID = process.argv[2];
 
-  const url = "https://swapi-api.alx-tools.com/api/films/" + movieID;
+  const url = 'https://swapi-api.alx-tools.com/api/films/' + movieID;
   request(url, (error, response, body) => {
     const People = JSON.parse(body).characters;
     People.forEach((PeopleURL) => {
       request(PeopleURL, (err, res, bdy) => {
         const ChName = JSON.parse(bdy).name;
         console.log(ChName);
+        if (err) {
+          console.log(err);
+        }
       });
     });
     if (error) {

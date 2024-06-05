88ffd017a4e268c46a2a1ddf77dd81a05513be1d
robberbadeia <robber.badeia@gmail.com>
2024-06-05 19:52:02 +0300

    Task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,23 @@
 #!/usr/bin/node
-// Start Wars Charachters
 
 const request = require('request');
 
-const argCounts = process.argv.length;
-if (argCounts < 3) {
-  console.log('Please Enter Movie ID');
-} else {
-  const movieID = process.argv[2];
+const movieId = process.argv[2];
 
-  const url = 'https://swapi-api.alx-tools.com/api/films/' + movieID;
-  request(url, (error, response, body) => {
-    const People = JSON.parse(body).characters;
-    People.forEach((PeopleURL) => {
-      request(PeopleURL, (err, res, bdy) => {
-        const ChName = JSON.parse(bdy).name;
-        console.log(ChName);
-        if (err) {
-          console.log(err);
-        }
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, async (err, res, body) => {
+  err && console.log(err);
+
+  const charactersArray = (JSON.parse(res.body).characters);
+  for (const character of charactersArray) {
+    await new Promise((resolve, reject) => {
+      request(character, (err, res, body) => {
+        err && console.log(err);
+
+        console.log(JSON.parse(body).name);
+        resolve();
       });
     });
-    if (error) {
-      console.log(error);
-    }
-  });
-}
+  }
+});

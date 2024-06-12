1832f96ac025102f13dcf7f1615052ee8436ccb7
robberbadeia <robber.badeia@gmail.com>
2024-06-12 12:51:30 +0300

    Task 0

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Task 0
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Function
+    """
+    n = len(matrix)
+
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
